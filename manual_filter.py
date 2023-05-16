f76e43598710aeb9eb704f8fa27837af44eb6e11
James Boland <jamespatrickboland@gmail.com>
2023-05-16 22:33:49 +0200

    Creating a new class for storing and retrieving data

--- manual_filter.py
+++ manual_filter.py
@@ -4,6 +4,32 @@ NUM_PARAMS = 3
 LEARNING_RATE = 0.1
 EPSILON = 0.1
 
+
+class DataIndex:
+    def __init__(self):
+        self.ratings = {}
+        self.movie_users = {}
+        self.user_movies = {}
+    
+    def add_rating(self, user, movie, rating):
+        self.ratings[(user, movie)] = rating
+        if movie not in self.movie_users:
+            self.movie_users[movie] = set()
+        self.movie_users[movie].add(user)
+        if user not in self.user_movies:
+            self.user_movies[user] = set()
+        self.user_movies[user].add(movie)
+
+    def get_users_for_movie(self, movie):
+        return self.movie_users[movie]
+    
+    def get_movies_for_user(self, user):
+        return self.user_movies[user]
+    
+    def get_ratings(self, user, movie):
+        return self.ratings[(user, movie)]
+    
+
 def main():
     ratings, users, movies = load_data()
     user_params, movie_params = initialise_params(users, movies)
